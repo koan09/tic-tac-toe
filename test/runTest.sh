54a7fcd31a4affc44ad1f3480b3eb38559c69adb
Koen den Hertog <koan09@hotmail.com>
2023-10-31 10:08:10 +0100

    run all test with runTest.sh <command>

---
+++ test/runTest.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+if [ -z $1 ] ; then       # if first command is empty 
+    echo arg 1 needs to be the programming runnning on the test files.
+    exit 1 
+fi
+
+for file in $(dirname $0)/*.test ; do
+        echo -n "$file: "; cat $file; cat $file | $1  
+done
+
+
