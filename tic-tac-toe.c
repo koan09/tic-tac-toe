5fb16081d7c5721a51b010028f154f5029946098
purna <purna.bishas@gmail.com>
2023-10-31 11:16:21 +0100

    added prettyPrint and modified cheat detection

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -2,6 +2,8 @@
 
 int readInput(FILE* input, char* plek);
 char winner(char* plek);
+int prettyPrint(char* plek);
+int cheatDetection(char* plek, int length);
 
 int main(){
 	extern FILE* stdin;
@@ -10,16 +12,47 @@ int main(){
 	if(readInput(stdin, lijst)){
 		return 1;
 	}
-
+	prettyPrint(lijst);
+	cheatDetection(lijst, 11);
 	printf("%c\n", winner(lijst));
 	return 0;
 }
 
+int prettyPrint(char* plek){
+	printf(" %c | %c | %c \n", plek[0], plek[1], plek[2]);
+	printf("-----------\n");
+	printf(" %c | %c | %c \n", plek[3],plek[4],plek[5]);
+	printf("-----------\n");
+	printf(" %c | %c | %c \n", plek[6],plek[7],plek[8]);
+	return 0;
+}
+
+int cheatDetection(char* plek, int length){
+	int counterX = 0;
+	int counterO = 0;
+	
+
+	for(int i=0; i < length; i++){
+		if (plek[i] == 'X'){
+			counterX ++;
+		}
+		if (plek[i] == 'O'){
+			counterO ++;
+		}
+	}
+
+	if (counterX ==6 || (counterX - counterO) >= 2) {
+		printf("X Cheated \n");
+	}
+
+	if (counterO ==6 || (counterO - counterX) >= 2) {
+		printf("O Cheated \n");
+	}
+	return 0;
+}
 
 int readInput(FILE* input, char* plek){
 	int i = 0;
-	int counterX = 0;
-	int counterO = 0;
 	char c;
 	extern FILE* stderr;
 
@@ -30,28 +63,10 @@ int readInput(FILE* input, char* plek){
 			fputs("Error, too many chars\n", stderr);
 			return 1;
 		}
-		if (c == 'X'){
-			counterX ++;
-			if (counterX >= 5){
-			printf ("X played unfair \n");
-			}
-		}
-		else{
-			return 0;
-		}
-		if (c == 'O'){
-			counterX ++;
-				if (counterO >= 5){
-				printf ("O played unfair \n");
-				}
-		}
-		else {
-			return 0;
-		}
-
+	}
 	plek[10] = '\0';
 	return 0;
-}
+
 }
 
 char winner(char* plek){
